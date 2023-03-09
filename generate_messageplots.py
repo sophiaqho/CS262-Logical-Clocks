a149f53328416e79708ed412a40d8a8aa085309c
Sanjana Singh <sanjanasingh@college.harvard.edu>
2023-03-08 19:45:29 -0500

    graphed trials

--- generate_messageplots.py
+++ generate_messageplots.py
@@ -9,10 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load the message log files
-filepath = "/Users/sanjana/Documents/GitHub/CS262-Logical-Clocks/"
+filepath = "/Users/sanjana/Documents/GitHub/CS262-Logical-Clocks/graphed_trial2/"
 filename1 = "MessageLog_1.txt"
 filename2 = "MessageLog_2.txt"
 filename3 = "MessageLog_3.txt"
+outfile1 = "MessageQueue_1.png"
+outfile2 = "MessageQueue_2.png"
+outfile3 = "MessageQueue_3.png"
 
 array1 = np.loadtxt(filepath + filename1)
 array2 = np.loadtxt(filepath + filename2)
@@ -25,19 +28,30 @@ time_1 = array1[0]
 time_2 = array2[0]
 time_3 = array3[0]
 
-# generate arrays with 120 entries per second
-# LCD(1, 2, 3, 4, 5, 6) = 120; every operation
-# takes place on an instance
-def generate_larger_arrays(array1, array2, array3):
-    inflated_len_1 = (len(array1) * 120) / array1[0]
-    inflated_len_2 = (len(array2) * 120) / array2[0]
-    inflated_len_3 = (len(array3) * 120) / array3[0]
-
-    # TODO- ss
-
-# manipulate the arrays
-large_1_array, large_2_array, large_3_array = generate_larger_arrays(array1, array2, array3)
-
+label_machine_1 = str(time_1) + " operations per second"
+label_machine_2 = str(time_2) + " operations per second"
+label_machine_3 = str(time_3) + " operations per second"
 
 # want to graph these
-plt.plot()
+plt.plot(array1[1:])
+plt.xlabel("Logical Clock Time")
+plt.ylabel("Length of Message Queue")
+plt.yticks(np.arange(0, 25, 1.0))
+plt.title(label_machine_1)
+plt.savefig(filepath + outfile1)
+plt.clf()
+
+plt.plot(array2[1:])
+plt.xlabel("Logical Clock Time")
+plt.ylabel("Length of Message Queue")
+plt.yticks(np.arange(0, 25, 1.0))
+plt.title(label_machine_2)
+plt.savefig(filepath + outfile2)
+plt.clf()
+
+plt.plot(array3[1:])
+plt.xlabel("Logical Clock Time")
+plt.ylabel("Length of Message Queue")
+plt.yticks(np.arange(0, 25, 1.0))
+plt.title(label_machine_3)
+plt.savefig(filepath + outfile3)
